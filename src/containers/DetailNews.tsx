210f983dcc74d011ea8111f9ffeec512bd4d3dd1
mayank dwivedi <anragbetu@gmail.com>
2022-10-28 16:18:56 +0530

    fix for the

--- src/containers/DetailNews.tsx
+++ src/containers/DetailNews.tsx
@@ -1,13 +1,31 @@
 import { Divider, PageHeader, Tag } from "antd";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useLocation, useNavigate } from "react-router-dom";
 import Home from "./Home";
 
 const NewsDetail = () => {
+  const [ApiResponse, setApiResponse] = useState<any>({});
   const { state, pathname } = useLocation();
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
+  useEffect(() => {
+    fetchCall();
+  }, []);
+
+  const fetchCall = async () => {
+    let slug = pathname.split("/")[2];
+    slug = slug.replaceAll("-", " ");
+    let response = await axios({
+      method: "get",
+      url: "https://dailycrimenews.in/readNews",
+    });
+    let fetchedResponse: [] = response.data.news;
+    let article = fetchedResponse.filter((e: any) => e?.urlSlug === slug);
+    console.log(article[0]);
+    setApiResponse(article[0]);
+  };
   return (
     <React.Fragment>
       <PageHeader
@@ -21,7 +39,9 @@ const NewsDetail = () => {
         className="site-page-header"
         onBack={() => navigate("/")}
         title={"Back"}
-        subTitle={<Tag color="red"> {state?.date}</Tag>}
+        subTitle={
+          <Tag color="red"> {state?.date || ApiResponse?.createdAt}</Tag>
+        }
       />
       <Helmet>
         <meta charSet="utf-8" />
@@ -30,25 +50,31 @@ const NewsDetail = () => {
       </Helmet>
       <div className="detail-base">
         <div className="detail-page">
-          <div className="detail-headline hindi-font">{state?.heading}</div>
+          <div className="detail-headline hindi-font">
+            {state?.heading || ApiResponse?.Heading}
+          </div>
         </div>
 
         <div className="detail-img">
           <img
-            src={state?.imgUrl}
+            src={state?.imgUrl || ApiResponse?.imageurl}
             style={{ margin: "10px", width: "100%" }}
-            alt={state?.heading}
+            alt={state?.heading || ApiResponse?.Heading}
           />
         </div>
         <div className="detail-meta">
-          <meta about={state?.content}></meta>
+          <meta about={state?.content || ApiResponse?.detail}></meta>
           <div>
-            <Tag color="blue">{state?.author}</Tag>
-            <Tag color="blue">{state?.date}</Tag>
+            {state?.author && <Tag color="blue">{state?.author}</Tag>}
+            {state?.date && <Tag color="blue">{state?.date}</Tag>}
             {state?.location && <Tag color="red">{state?.location}</Tag>}
           </div>
         </div>
-        <td dangerouslySetInnerHTML={{ __html: state?.detailContent }} />
+        <td
+          dangerouslySetInnerHTML={{
+            __html: state?.detailContent || ApiResponse?.detail,
+          }}
+        />
         {/* <div className="detail-news hindi-font">{state?.detailContent}</div> */}
       </div>
 
