593f2ed9db1ff7fe840d71fa6d119600d3b6cf46
mayank dwivedi <anragbetu@gmail.com>
2022-10-30 02:12:30 +0530

    foex

--- pages/admin.js
+++ pages/admin.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import HTMLEditor from "../components/Html";
 import { gql, useMutation } from "@apollo/client";
 import { message } from "antd";
+import Image from "next/image";
 
 const Admin = () => {
   const ADD_ARTICLE = gql`
@@ -122,39 +123,43 @@ const Admin = () => {
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
-                >
-                  {featuredImage ? `Image URL- ` + featuredImage : ""}
-                </label>
+                {featuredImage && (
+                  <Image
+                    src={featuredImage}
+                    width={190}
+                    height={190}
+                    alt="upload"
+                  />
+                )}
               </div>
             </div>
 
-            <div>
-              <button
-                onClick={onSubmit}
-                type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg
-                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </span>
-                Upload
-              </button>
-            </div>
+            {featuredImage && (
+              <div>
+                <button
+                  onClick={onSubmit}
+                  type="submit"
+                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg
+                      className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  Upload
+                </button>
+              </div>
+            )}
           </form>
         </div>
       </div>
