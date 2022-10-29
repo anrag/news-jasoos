4c65928c493a97606a3f29bdec7b4cf237b3c854
mayank dwivedi <anragbetu@gmail.com>
2022-10-29 22:53:31 +0530

    fix for the GA id

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,24 @@ import { ApolloProvider } from "@apollo/client";
 import "../styles/globals.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import client from "./api/graphql-client";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PLWLTD0PEE');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ApolloProvider>
   );
