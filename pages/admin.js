622688d05b344781e40df9bd18bcc503e9f714bd
mayank dwivedi <anragbetu@gmail.com>
2022-10-29 18:42:50 +0530

    fox

--- pages/admin.js
+++ pages/admin.js
@@ -80,27 +80,27 @@ const Admin = () => {
 
   return (
     <>
-      <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
           <div>
             <img
-              class="mx-auto h-12 w-auto"
+              className="mx-auto h-12 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Write News
             </h2>
           </div>
-          <form class="mt-8 space-y-6">
+          <form className="mt-8 space-y-6">
             <input type="hidden" name="remember" value="true" />
-            <div class="-space-y-px rounded-md shadow-sm">
+            <div className="-space-y-px rounded-md shadow-sm">
               <div>
                 <textarea
                   id="email-address"
                   onChange={(e) => setTitle(e.target.value)}
                   required
-                  class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="title"
                 />
               </div>
@@ -110,7 +110,7 @@ const Admin = () => {
               <textarea
                 required
                 onChange={(e) => setShortDesc(e.target.value)}
-                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                 placeholder="Enter Short detail"
               />
             </div>
@@ -123,11 +123,11 @@ const Admin = () => {
               <input type="file" accept="image/*" onChange={onImageChange} />
             </div>
 
-            <div class="flex items-center justify-between">
-              <div class="flex items-center">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
                 <label
                   for="remember-me"
-                  class="ml-2 block text-sm text-gray-900"
+                  className="ml-2 block text-sm text-gray-900"
                 >
                   {featuredImage ? `Image URL- ` + featuredImage : ""}
                 </label>
@@ -138,11 +138,11 @@ const Admin = () => {
               <button
                 onClick={onSubmit}
                 type="submit"
-                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <svg
-                    class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="currentColor"
