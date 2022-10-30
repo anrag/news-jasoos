de1e3f1fd1a7277dab4bbb0f6d62d8109794fbfa
mayank dwivedi <anragbetu@gmail.com>
2022-10-30 13:50:12 +0530

    fix for the sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,58 @@
+import { gql } from "@apollo/client";
+import React from "react";
+import client from "./api/graphql-client";
+
+const EXTERNAL_DATA_URL = "https://newsjasoos.in/read";
+
+function createSitemap(response) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${response
+    .map(({ id }, index) => {
+      return `
+          <url>
+              <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+          </url>
+      `;
+    })
+    .join("")}
+  </urlset>`;
+}
+
+class Sitemap extends React.Component {
+  static async getInitialProps({ res }) {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept-Language", "en-US,en;q=0.9,hi-IN;q=0.8,hi;q=0.7");
+    myHeaders.append("Connection", "keep-alive");
+    myHeaders.append("Origin", "https://newsjasoos.in");
+    myHeaders.append("Referer", "https://newsjasoos.in/");
+    myHeaders.append("Sec-Fetch-Dest", "empty");
+    myHeaders.append("Sec-Fetch-Mode", "cors");
+    myHeaders.append("Sec-Fetch-Site", "cross-site");
+
+    myHeaders.append("accept", "*/*");
+    myHeaders.append("content-type", "application/json");
+
+    var raw = JSON.stringify({
+      variables: {},
+      query:
+        "{\n  article {\n    count\n    data {\n      id\n      title\n          }\n     }\n}",
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    let dat = await fetch("https://dailycrimenews.in/", requestOptions);
+    dat = await dat.json();
+    dat = dat.data.article.data;
+    res.setHeader("Content-Type", "text/xml");
+    res.write(createSitemap(dat));
+    res.end();
+  }
+}
+
+export default Sitemap;
