780a7a761e88256a9c18dfad019b04611142aacb
mayank dwivedi <anragbetu@gmail.com>
2022-10-30 12:18:12 +0530

    added

--- pages/read/[pid].js
+++ pages/read/[pid].js
@@ -77,79 +77,102 @@ const Post = () => {
         <meta property="og:locale" content="hi" />
       </Head> */}
       <Suspense fallback={<p>Loading feed...</p>}>
-        <div className="bg-white dark:bg-slate-800 flex flex-col">
-          <div className="flex  sticky top-0 z-50">
-            <div className="basis-1/2 p-5">
-              <Link href={"/"} des passHref>
-                <Button
-                  style={{ borderRadius: 10 }}
-                  type="primary"
-                  title="Back"
-                  href="/"
-                >
-                  Back
-                </Button>
-              </Link>
-            </div>
-            <div>
-              <SideNavbar />
-            </div>
+        {detail?.title === null ? (
+          <div className="grid h-screen place-items-center">
+            <button
+              type="button"
+              style={{
+                fontSize: "30px",
+                padding: "20px",
+                borderRadius: "10px",
+              }}
+              className="text-white bg-indigo-500"
+              disabled
+            >
+              <svg
+                className="animate-spin h-5 w-5 mr-3"
+                viewBox="0 0 24 24"
+              ></svg>
+              ✋ कृपया प्रतीक्षा करें ✋...
+            </button>
           </div>
-          <div className="place-items-center">
-            <div className="content-center text-black dark:text-slate-100">
-              <h1 className="text-black dark:text-slate-100">
-                {detail?.title}
-              </h1>
-            </div>
-
-            <div>
-              {data?.article.data[0]?.featureImage && (
-                <Image
-                  className="rounded-t-lg"
-                  style={{ height: 200, width: "100%" }}
-                  height={80}
-                  width={"100%"}
-                  layout="responsive"
-                  src={data?.article.data[0]?.featureImage}
-                  alt={detail?.title}
-                />
-              )}
-            </div>
-            <div>
-              <div>
-                <div className="flex p-2">
-                  <Tag color="gold">अवनीश चौधरी</Tag>
-                  <Tag color="black">
-                    {moment(
-                      new Date(parseInt(detail?.createdAt)).toString()
-                    ).format("DD/MM/YY hh:mm A")}
-                  </Tag>
+        ) : (
+          <>
+            <div className="bg-white dark:bg-slate-800 flex flex-col">
+              <div className="flex  sticky top-0 z-50">
+                <div className="basis-1/2 p-5">
+                  <Link href={"/"} des passHref>
+                    <Button
+                      style={{ borderRadius: 10 }}
+                      type="primary"
+                      title="Back"
+                      href="/"
+                    >
+                      Back
+                    </Button>
+                  </Link>
+                </div>
+                <div>
+                  <SideNavbar />
                 </div>
               </div>
-              <div
-                className="desktop:m-100 laptop:m-100 article-text text-black dark:text-slate-100"
-                dangerouslySetInnerHTML={{ __html: detail?.longArticle }}
-              />
+              <div className="place-items-center">
+                <div className="content-center text-black dark:text-slate-100">
+                  <h1 className="text-black dark:text-slate-100">
+                    {detail?.title}
+                  </h1>
+                </div>
+
+                <div>
+                  {data?.article.data[0]?.featureImage && (
+                    <Image
+                      className="rounded-t-lg"
+                      style={{ height: 200, width: "100%" }}
+                      height={80}
+                      width={"100%"}
+                      layout="responsive"
+                      src={data?.article.data[0]?.featureImage}
+                      alt={detail?.title}
+                    />
+                  )}
+                </div>
+                <div>
+                  <div>
+                    <div className="flex p-2">
+                      <Tag color="gold">अवनीश चौधरी</Tag>
+                      <Tag color="black">
+                        {moment(
+                          new Date(parseInt(detail?.createdAt)).toString()
+                        ).format("DD/MM/YY hh:mm A")}
+                      </Tag>
+                    </div>
+                  </div>
+                  <div
+                    className="desktop:m-100 laptop:m-100 article-text text-black dark:text-slate-100"
+                    dangerouslySetInnerHTML={{ __html: detail?.longArticle }}
+                  />
+                </div>
+                <div>
+                  {data?.article.data[0]?.secondaryImage && (
+                    <Image
+                      className="rounded-t-lg"
+                      style={{ height: 200, width: "100%" }}
+                      height={80}
+                      width={"100%"}
+                      layout="responsive"
+                      priority
+                      src={data?.article.data[0]?.secondaryImage}
+                      alt={detail?.title}
+                    />
+                  )}
+                </div>
+              </div>
             </div>
-            <div>
-              {data?.article.data[0]?.secondaryImage && (
-                <Image
-                  className="rounded-t-lg"
-                  style={{ height: 200, width: "100%" }}
-                  height={80}
-                  width={"100%"}
-                  layout="responsive"
-                  priority
-                  src={data?.article.data[0]?.secondaryImage}
-                  alt={detail?.title}
-                />
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="p-10 text-center bg-white dark:bg-slate-800 text-slate-500 dark:text-slate-100">
-          <p className="bigger-text">और पड़े</p>
-        </div>
+            <div className="p-10 text-center bg-white dark:bg-slate-800 text-slate-500 dark:text-slate-100">
+              <p className="bigger-text">और पड़े</p>
+            </div>{" "}
+          </>
+        )}
       </Suspense>
       <Suspense fallback={<p>Loading..</p>}>
         <Home />
