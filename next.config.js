e2e4816041ba8b90599c65a9a656f7dd0e8a62db
mayank dwivedi <anragbetu@gmail.com>
2022-10-31 19:26:32 +0530

    white listed Images

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   runtimeCaching,
+
   // buildExcludes: [/middleware-manifest.json$/],
 });
 
 const nextConfig = withPWA({
   // next config
   distDir: "build",
+  images: {
+    domains: ["my-usa-cricket.s3.amazonaws.com"],
+  },
 });
 module.exports = nextConfig;
