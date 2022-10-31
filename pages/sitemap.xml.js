48a4bb498db6a7081f8eb2e8de173d5c8c043d04
mayank dwivedi <anragbetu@gmail.com>
2022-11-01 02:30:57 +0530

    changes format for the list

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -55,11 +55,11 @@ class Sitemap extends React.Component {
       let dat = await fetch("https://dailycrimenews.in/", requestOptions);
       dat = await dat.json();
       dat = dat?.data?.article?.data;
-      res?.setHeader("Content-Type", "text/xml");
+      res?.setHeader("Content-Type", "application/xml");
       res?.write(createSitemap(dat));
       res?.end();
     } catch (error) {
-      res?.setHeader("Content-Type", "text/xml");
+      res?.setHeader("Content-Type", "application/xml");
       res?.write(createSitemap([]));
       res.end();
     }
