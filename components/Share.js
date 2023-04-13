a71c5ba57c873002b4b1dcfdc8ed6a62b99b7b44
mayank dwivedi <118843025+milkgarage@users.noreply.github.com>
2023-04-14 00:03:17 +0530

    message

--- components/Share.js
+++ components/Share.js
@@ -4,16 +4,14 @@ export const share = async (title, url, text, imgUrl) => {
   message.destroy();
   message.loading("✋ कृपया प्रतीक्षा करें ✋");
   // Convert dataUrl into blob using browser fetch API
-  const blob = await (await fetch(imgUrl,{
-    mode:'no-cors'
-  })).blob();
+  // const blob = await (await fetch(imgUrl)).blob();
 
   // Create file form the blob
-  const image = new File([blob], "canvas.png", { type: blob.type });
+  // const image = new File([blob], "canvas.png", { type: blob.type });
   if (navigator.share) {
     navigator.share({
       title: `${title}`,
-      files: [image],
+      // files: [image],
       url: `https://newsjasoos.in${url}`,
       text: `${text.substring(0, 400)} \n
         पूरी खबर पढ़ने के लिए 👇 क्लिक करें \n`,
